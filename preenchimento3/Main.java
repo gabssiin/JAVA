809d208d6d87f80fd6026cee638232fbad4db74e
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2024-10-09 00:20:12 +0000

    preen3

---
+++ preenchimento3/Main.java
@@ -0,0 +1,42 @@
+/*Leia um valor X. Coloque este valor na primeira posição de um vetor N[100]. Em cada posição subsequente de N (1 até 99), coloque a metade do valor armazenado na posição anterior, conforme o exemplo abaixo. Imprima o vetor N.
+
+Entrada
+A entrada contem um valor de dupla precisão com 4 casas decimais.
+
+Saída
+Para cada posição do vetor N, escreva "N[i] = Y", onde i é a posição do vetor e Y é o valor armazenado naquela posição. Cada valor do vetor deve ser apresentado com 4 casas decimais. */
+
+package preenchimento3;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    double X = ler.nextDouble();
+    int[] N = new int[100];
+
+    for(int i=0; i<100; ++i){
+    
+    }
+    
+    for(int i=0; i<100; ++i){
+
+
+    }
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+ler.close();
+    }
+}
