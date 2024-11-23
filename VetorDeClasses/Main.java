bbc927d2efb771fd20387d3d3269118fd5d73340
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2024-11-23 19:14:23 +0000

    tarefa

--- VetorDeClasses/Main.java
+++ VetorDeClasses/Main.java
@@ -1,5 +1,21 @@
 package VetorDeClasses;
+/*
+ * Faça um programa que leia os dados de 10 alunos (Nome, matricula, Média Final), armazenando em um vetor.
+Uma vez lidos os dados, divida estes dados em 2 novos vetores, o vetor dos aprovados e o vetor dos reprovados, considerando a média mínima para a aprovação como sendo 
+.5,0.
+Exibir na tela os dados do vetor de aprovados, seguido dos dados do vetor de reprovados.
+
+Anexar:
+arquivos .java com os seus códigos
+arquivo .txt para executar o seu código
+vídeo de até 1 minuto executando o seu código
+
+ */
+
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+
+    }
 }
