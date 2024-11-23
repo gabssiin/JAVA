39927658ade9ac603153639f7b4d0b17c959f61f
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2024-11-23 18:35:14 +0000

    resolvido

--- testesdeSala/Alunos.java
+++ testesdeSala/Alunos.java
@@ -1,22 +1,77 @@
 package testesdeSala;
 
-public class Alunos{
+public class Alunos {
     String nome;
     int matricula;
     double nota1;
     double nota2;
     double nota3;
 
-  
-    Alunos() {
+    public Alunos() {
+        // Construtor padrão - inicializa com valores padrão se necessário
     }
-  
-    Alunos(String nome, int matricula, double nota1, double nota2, double nota3) {
-      this.nome = nome;
-      this.matricula = matricula;
-      this.nota1 = nota1;
-      this.nota2 = nota2;
-      this.nota3 = nota3;
 
+    public Alunos(String nome, int matricula, double nota1, double nota2, double nota3) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
     }
-  }
+
+    public double calcularMedia() {
+        return (nota1 + nota2 + nota3) / 3.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome +
+               ", Matrícula: " + matricula +
+               ", Nota 1: " + nota1 +
+               ", Nota 2: " + nota2 +
+               ", Nota 3: " + nota3 +
+               ", Média: " + calcularMedia();
+    }
+
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {  // Getter para nota3
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {  // Setter para nota3
+        this.nota3 = nota3;
+    }
+}
--- testesdeSala/QuartaQuestão.java
+++ testesdeSala/QuartaQuestão.java
@@ -1,23 +1,101 @@
 package testesdeSala;
 
-/* Crie uma estrutura representando os alunos de um curso. A estrutura debe conter a matricula, nome, nota da primeira prova, nota da segunda e nota da terceira prova. 
-A. Permita o input
-B. Encontre o aluno com maiior nota da primeira
-c. == maior média geraç
-D. == menor média geral
-E. Para cada aulo diga se ele foi aprovado ou reprovado considaderando o 6 de média.
-} */
 import java.util.Scanner;
 
 public class QuartaQuestão {
     public static Scanner ler = new Scanner(System.in);
     public static final int numDeAlunos = 5;
+
     public static void main(String[] args) {
-        Alunos[] Alunos = new Alunos[numDeAlunos];
-        lerAlunos();
-        
-            
-            private static void lerAlunos() {
-                
+        Alunos[] alunos = new Alunos[numDeAlunos];
+
+        lerAlunos(alunos); // (A) Ler os dados dos alunos
+
+        Alunos alunoMaiorNota1 = encontrarAlunoMaiorNota1(alunos); // (B) Encontrar aluno com maior nota na primeira prova
+        System.out.println("Aluno com maior nota na primeira prova: " + alunoMaiorNota1);
+
+
+        Alunos alunoMaiorMedia = encontrarAlunoMaiorMedia(alunos); // (C) Encontrar aluno com maior média
+        System.out.println("Aluno com maior média: " + alunoMaiorMedia);
+
+        Alunos alunoMenorMedia = encontrarAlunoMenorMedia(alunos); // (D) Encontrar aluno com menor média
+        System.out.println("Aluno com menor média: " + alunoMenorMedia);
+
+
+        imprimirResultados(alunos); // (E) Imprimir resultados e status de aprovação
+
+        ler.close();
+    }
+
+
+
+    public static void lerAlunos(Alunos[] alunos) {
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("Digite os dados do aluno " + (i + 1) + ":");
+            alunos[i] = lerAluno();
+        }
+    }
+
+
+    public static Alunos lerAluno() {
+        System.out.print("Nome: ");
+        String nome = ler.next(); // Use next() para ler apenas o nome
+        System.out.print("Matrícula: ");
+        int matricula = ler.nextInt();
+        System.out.print("Nota 1: ");
+        double nota1 = ler.nextDouble();
+        System.out.print("Nota 2: ");
+        double nota2 = ler.nextDouble();
+        System.out.print("Nota 3: ");
+        double nota3 = ler.nextDouble();
+        ler.nextLine(); // Limpar o buffer do scanner
+        return new Alunos(nome, matricula, nota1, nota2, nota3);
+    }
+
+
+    public static Alunos encontrarAlunoMaiorNota1(Alunos[] alunos) {
+        Alunos maiorNota1 = alunos[0];
+        for (int i = 1; i < alunos.length; i++) {
+            if (alunos[i].nota1 > maiorNota1.nota1) {
+                maiorNota1 = alunos[i];
             }
+        }
+        return maiorNota1;
+    }
+
+
+
+
+    public static Alunos encontrarAlunoMaiorMedia(Alunos[] alunos) {
+        Alunos maiorMedia = alunos[0];
+        for (int i = 1; i < alunos.length; i++) {
+            if (alunos[i].calcularMedia() > maiorMedia.calcularMedia()) {
+                maiorMedia = alunos[i];
+            }
+        }
+        return maiorMedia;
+    }
+
+
+    public static Alunos encontrarAlunoMenorMedia(Alunos[] alunos) {
+        Alunos menorMedia = alunos[0];
+        for (int i = 1; i < alunos.length; i++) {
+            if (alunos[i].calcularMedia() < menorMedia.calcularMedia()) {
+                menorMedia = alunos[i];
+            }
+        }
+        return menorMedia;
+    }
+
+    public static void imprimirResultados(Alunos[] alunos) {
+        for (Alunos aluno : alunos) {
+            System.out.println(aluno);
+            if (aluno.calcularMedia() >= 6.0) {
+                System.out.println("Aprovado");
+            } else {
+                System.out.println("Reprovado");
+            }
+            System.out.println("--------------------");
+        }
+    }
 }
