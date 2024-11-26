2074e5ba9b39d459756c29009d808d9fead25e48
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2024-11-26 22:38:49 +0000

    cuida

---
+++ VetorDeClasses/Aluno.java
@@ -0,0 +1,13 @@
+package VetorDeClasses;
+
+class Aluno {
+    String nome;
+    int matricula;
+    double mediaFinal;
+
+    public Aluno(String nome, int matricula, double mediaFinal) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.mediaFinal = mediaFinal;
+    }
+}
--- VetorDeClasses/Main.java
+++ VetorDeClasses/Main.java
@@ -12,10 +12,58 @@ vídeo de até 1 minuto executando o seu código
 
  */
 
+ import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+ 
+ 
+ public class Main {
+ 
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         
         
-
-    }
-}
+         Aluno[] alunos = new Aluno[10];
+         Aluno[] aprovados = new Aluno[10];
+         Aluno[] reprovados = new Aluno[10];
+         
+         int aprovadosCount = 0;
+         int reprovadosCount = 0;
+         
+        
+         for (int i = 0; i < 10; i++) {
+             System.out.println("Digite o nome do aluno " + (i+1) + ":");
+             String nome = sc.nextLine();
+             
+             System.out.println("Digite a matrícula do aluno " + (i+1) + ":");
+             int matricula = sc.nextInt();
+             
+             System.out.println("Digite a média final do aluno " + (i+1) + ":");
+             double mediaFinal = sc.nextDouble();
+             sc.nextLine(); 
+             
+             
+             alunos[i] = new Aluno(nome, matricula, mediaFinal);
+             
+            
+             if (mediaFinal >= 5.0) {
+                 aprovados[aprovadosCount++] = alunos[i];
+             } else {
+                 reprovados[reprovadosCount++] = alunos[i];
+             }
+         }
+         
+         System.out.println("\nAlunos Aprovados:");
+         for (int i = 0; i < aprovadosCount; i++) {
+             System.out.println("Nome: " + aprovados[i].nome + ", Matrícula: " + aprovados[i].matricula + ", Média: " + aprovados[i].mediaFinal);
+         }
+ 
+ 
+         System.out.println("\nAlunos Reprovados:");
+         for (int i = 0; i < reprovadosCount; i++) {
+             System.out.println("Nome: " + reprovados[i].nome + ", Matrícula: " + reprovados[i].matricula + ", Média: " + reprovados[i].mediaFinal);
+         }
+         
+         sc.close();
+     }
+ }
+ 
